c5056416cf89ec3a06ab2322cd5ccb4ed32554f9
Strel79 <a.strelins@mail.ru>
2024-07-21 17:05:16 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -3,11 +3,11 @@ def get_matrix(n, m, value):
      for i in range(n):
          matrix.append([0] * m)
          for j in range(n):
-             matrix.append([value]*m)
+             matrix.append([value] * m)
          return(matrix)
-result1 = get_matrix(2,2,10)
+result1 = get_matrix(2, 2, 10)
 print(result1)
-result2 = get_matrix(3,5, 42)
+result2 = get_matrix(3, 5, 42)
 print(result2)
-result3 = get_matrix(4,2, 13)
+result3 = get_matrix(4, 2, 13)
 print(result3)
